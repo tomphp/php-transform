7823129edd81aba5616be362cf41544b25284df1
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2016-01-16 18:15:50 +0100

    Throws exceptions in func & inside transformation
    When a wrong argument is passed to a function,
    `InvalidArgumentException` is thrown. When the transformation callback
    receives a bad argument a `UnexpectedValueException` is thrown.
    
    See #5

--- src/transform.php
+++ src/transform.php
@@ -3,6 +3,7 @@
 namespace TomPHP\Transform;
 
 use TomPHP\Transform\Exception\InvalidArgumentException;
+use TomPHP\Transform\Exception\UnexpectedValueException;
 
 /**
  * Represents a placeholder in an argument list.
@@ -17,7 +18,7 @@ const __ = 'Super Unique Placeholder String - 6f74f07a-bc60-494a-93e0-eefedb6984
  *
  * @return \Closure
  */
-function chain(...$fns)
+function chain(callable ...$fns)
 {
     return function ($value) use ($fns) {
         return array_reduce(
@@ -45,13 +46,23 @@ function __()
  * the result.
  *
  * @param string $methodName
- * @param array  $arguments
- *
+ * @param array  $args
  * @return \Closure
+ * @throws \TomPHP\Transform\Exception\ExpectedStringTrait
  */
 function callMethod($methodName, ...$args)
 {
+    if (!is_string($methodName)) {
+        throw InvalidArgumentException::expectedString('methodName', $methodName);
+    }
+
     return function ($object) use ($methodName, $args) {
+        if (!is_object($object)) {
+            throw UnexpectedValueException::expectedObject('object', $object);
+        } elseif (!method_exists($object, $methodName)) {
+            throw  UnexpectedValueException::expectedObject($object, $methodName);
+        }
+
         return $object->$methodName(...$args);
     };
 }
@@ -118,7 +129,7 @@ function getProperty($name)
  * Returns a transformer calls the given callable with its value as the
  * argument and returns the result.
  *
- * @param callable $name      Providing an array will walk multiple levels into
+ * @param callable $callable  Providing an array will walk multiple levels into
  *                            the array.
  * @param mixed[]  $arguments Use __ to indicate where the value should be
  *                            placed in the argument list.
@@ -143,8 +154,8 @@ function argumentTo(callable $callable, array $arguments = [__])
  * Returns a transformer which prepends $prefix onto its value.
  *
  * @param string $prefix
- *
  * @return \Closure
+ * @throws \TomPHP\Transform\Exception\ExpectedStringTrait
  */
 function prepend($prefix)
 {
@@ -153,6 +164,10 @@ function prepend($prefix)
     }
 
     return function ($value) use ($prefix) {
+        if (!is_string($value) && ! (is_object($value) && method_exists($value, '__toString'))) {
+            throw UnexpectedValueException::expectedString('value', $value);
+        }
+
         return $prefix.$value;
     };
 }
@@ -161,8 +176,8 @@ function prepend($prefix)
  * Returns a transformer which appends $suffix onto its value.
  *
  * @param string $suffix
- *
  * @return \Closure
+ * @throws \TomPHP\Transform\Exception\ExpectedStringTrait
  */
 function append($suffix)
 {
@@ -171,6 +186,10 @@ function append($suffix)
     }
 
     return function ($value) use ($suffix) {
+        if (!is_string($value) && ! (is_object($value) && method_exists($value, '__toString'))) {
+            throw UnexpectedValueException::expectedString('value', $value);
+        }
+
         return $value.$suffix;
     };
 }
